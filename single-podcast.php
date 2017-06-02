0891b6628b710ad36256e8fba1a739019b382d37
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-06-02 06:50:16 +0200

    Ajout d’une condition pour l’image une

--- single-podcast.php
+++ single-podcast.php
@@ -3,6 +3,9 @@
 // Image une après le titre
 add_action( 'genesis_entry_content', 'vft_image_avant_contenu', 5 );
 function vft_image_avant_contenu() {
+	// Si pas d'image une on s'arrête
+	if ( ! has_post_thumbnail() ) return;
+	// Affichag image une
 	the_post_thumbnail( 'full');
 }
 
