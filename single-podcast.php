4143c50dc1b1198fe4193f65d94c497fb840f44b
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-06-02 07:06:19 +0200

    Ajoutr vidéo après le contenu

--- single-podcast.php
+++ single-podcast.php
@@ -9,5 +9,15 @@ function vft_image_avant_contenu() {
 	the_post_thumbnail( 'full');
 }
 
+// Ajout de la vidéo après le contenu
+add_action( 'genesis_entry_content', 'vft_video_apres_contenu', 12 );
+function vft_video_apres_contenu() {
+	$video = get_field('url_de_la_video');
+	$oembed = wp_oembed_get( $video, array('width' => 800) );
+	if ( ! $video ) return;
+
+	echo '<h3>(re) Voir!</h3>';
+	echo $oembed;
+}
 
 genesis();
